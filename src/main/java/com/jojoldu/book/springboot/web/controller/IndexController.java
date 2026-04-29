d16f8fe6e9a4e866d49efe4bfe9fc60961d40b89
wonpyo12 <wonpyo0719@naver.com>
2026-04-29 15:36:25 +0900

    게시판 수정/삭제 추가

--- src/main/java/com/jojoldu/book/springboot/web/controller/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/controller/IndexController.java
@@ -1,10 +1,13 @@
 package com.jojoldu.book.springboot.web.controller;
 
+import com.jojoldu.book.springboot.domain.posts.PostsRepository;
 import com.jojoldu.book.springboot.service.posts.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +24,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsave() {
         return "posts_save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsupdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto= postsService.findById(id);
+        model.addAttribute("posts",dto);
+        return "posts_update";
     }
 }
