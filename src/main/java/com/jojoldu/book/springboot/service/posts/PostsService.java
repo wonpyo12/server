9e2f6bd09d9ae4fb3bc4f829b19d521b821e3dcd
wonpyo12 <wonpyo0719@naver.com>
2026-04-29 12:54:02 +0900

    게시판 SELECT(조회)추가

--- src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -1,12 +1,17 @@
 package com.jojoldu.book.springboot.service.posts;
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class PostsService {
@@ -63,5 +68,11 @@ public class PostsService {
 
         postsRepository.delete(posts);
 
+    }
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findALLDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/jojoldu/book/springboot/web/controller/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.jojoldu.book.springboot.web.controller;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
@@ -12,7 +13,8 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts",postsService.findAllDesc());
         return "index";
     }
 
